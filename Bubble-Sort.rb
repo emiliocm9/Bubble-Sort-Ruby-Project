1d26bc91d9f80ea2c6a1ff2550683d9e137aaff0
emiliocm9 <emilio.contreras97@gmail.com>
2020-05-05 15:45:10 -0500

    First code example

--- Bubble-Sort.rb
+++ Bubble-Sort.rb
@@ -1,1 +1,19 @@
-def 
+# frozen_string_literal: true
+
+def bubble_sort(arr)
+  num = arr.length
+    loop do
+     change = false
+      (num - 1).times do |i|
+         if arr[i] > arr[i + 1]
+        arr[i], arr[i + 1] = arr[i + 1], arr[i]
+        change = true
+        end
+      end
+     break if not change
+    end
+  arr
+end
+
+arr = [2, 3, 5, 2, 8, 7, 9, 5, 3, 4, 9, 7, 5, 5]
+print bubble_sort(arr)
